3bbc0368732c10cd2d5f5a156e46c1ed8794e60d
Odunayo Dauda <daudavictorodunayo@gmail.com>
2024-09-17 00:00:16 +0100

    Name edit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        name: 'Vite PWA Project',
-        short_name: 'Vite PWA Project',
+        name: 'Food Express',
+        short_name: 'Food Express',
         theme_color: '#ffffff',
         icons: [
             {
